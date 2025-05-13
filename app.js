436e3357057b91c3b957fbdf34c0d09327725b91
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-13 16:49:57 +0200

    Ho sistemato tutto il CRUD.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 //IMPORT DATA
-const posts = require("./data/array-blog");
+// const posts = require("./data/array-blog");
 
 //IMPORT ROUTER POSTS
 const routerPosts = require("./routers/posts");
@@ -16,9 +16,13 @@ const routerPosts = require("./routers/posts");
 app.use(express.static(`public`));
 
 //# SET RUTER POSTS --------------------------------
-app.use("/bacheca", routerPosts);
+app.use("/posts", routerPosts);
 
 //# LISTEN PORT --------------------------------
-app.listen(port, () => {
-  console.log(`Lettura della porta: ${port}`);
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Lettura della porta: ${port}`);
+  }
 });
--- routers/posts.js
+++ routers/posts.js
@@ -2,11 +2,12 @@
 const express = require("express");
 
 //IMPORT DATA
-const posts = require("../data/array-blog");
+let posts = require("../data/array-blog");
 
 //SET ROUTER
 const router = express.Router();
 
+// CRUD
 //# INDEX --------------------------------
 router.get("/", (req, res) => {
   res.json({
@@ -17,11 +18,49 @@ router.get("/", (req, res) => {
 
 //# SHOW --------------------------------
 router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
   res.json({
     message: `Visualizzo il post ${id}`,
     post,
+  });
+});
+
+//# STORE --------------------------------
+router.post("/", (req, res) => {
+  res.json({
+    message: "Aggiungo un post",
+  });
+});
+
+//# UPDATE --------------------------------
+router.put("/:id", (req, res) => {
+  res.json({
+    message: "Modifico un post",
+  });
+});
+
+//# MODIFY --------------------------------
+router.patch("/:id", (req, res) => {
+  res.json({
+    message: "Modifico un post",
+  });
+});
+
+//# DESTROY --------------------------------
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const error = posts.find((post) => post.id === id);
+
+  !error
+    ? res.send("Elemento non trovato")
+    : (posts = posts.filter((post) => post.id !== id));
+
+  res.json({
+    message: `Ho rimosso il post ${id}`,
+    posts,
   });
 });
 
